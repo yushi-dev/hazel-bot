31a0521ad31747deeb12f67afe3d0e16d03c6f38
yushi-dev <ja.agripo2003@gmail.com>
2021-06-03 11:47:11 +0200

    idk pls help

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -10,44 +10,23 @@ exports.run = ({ msg, args }) => {
         return;
     }
 
-    if (
-        Util.startsWithUsingArray(args[0].toLowerCase(), [
-            "can",
-            "will",
-            "is",
-            "are",
-            "am",
-            "do",
-            "how",
-            "does",
-            "would",
-            "may",
-            "might",
-            "should",
-        ]) &&
-        args[args.length - 1].endsWith("?") &&
-        args.length > 2
-    ) {
-        msg.channel.send(
-            Util.getRandomArrayElement([
-                "yes i agree",
-                "that's false",
-                "maybe",
-                "idk",
-                "not sure",
-                "idc",
-                "definitely",
-                "not answering",
-                "ur weird for asking that...",
-                "ask again :)",
-                "without a doubt",
-                "very likely",
-                "ofc",
-                "why're u asking that... pervert",
-                "u ask too many questions",
-            ])
-        );
-    } else {
-        msg.channel.send("that's not a question :(");
-    }
+    msg.channel.send(
+        Util.getRandomArrayElement([
+            "yes i agree",
+            "that's false",
+            "maybe",
+            "idk",
+            "not sure",
+            "idc",
+            "definitely",
+            "not answering",
+            "ur weird for asking that...",
+            "ask again :)",
+            "without a doubt",
+            "very likely",
+            "ofc",
+            "why're u asking that... pervert",
+            "u ask too many questions",
+        ])
+    );
 };
